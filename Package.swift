e2ac4935e0f351da0d2e3fed2b2fa6e74b4fbb56
David Ben-Yaakov <david@tangogolfdigital.com>
2022-10-29 14:52:09 -0500

    Updated for latest Scenes version
    * Updated to Igis 1.4.0, Scenes 1.1.6, and ScenesAnimations 1.0.0
    * Updated to swift-tools-version:5.7
    * Added newly required Swift Atomics

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 /*
@@ -32,11 +32,13 @@ let package = Package(
           targets: ["MerlinKarel"]),
     ],
     dependencies: [
+      .package(url: "https://github.com/apple/swift-atomics.git", from: "1.0.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "MerlinKarel"),
+          name: "MerlinKarel",
+          dependencies: [.product(name: "Atomics", package: "swift-atomics")])
     ]
 )
